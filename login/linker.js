e6ce516798aed7ee25aa8c10c5e5ad03e4146cfc
AleonDM <your.email@example.com>
2025-02-25 20:27:51 +0300

    1

--- login/linker.js
+++ login/linker.js
@@ -1,38 +1,47 @@
 const teams = [
     {
-        name: "Team spirit",
+        name: "spirit",
+        code: "team1",
         page: "1.html"
     },
     {
         name: "Team 2",
+        code: "team2",
         page: "2.html"
     },
     {
         name: "Team 3",
+        code: "team3",
         page: "3.html"
     },
     {
         name: "Team 4",
+        code: "team4",
         page: "4.html"
     },
     {
         name: "Team 5",
+        code: "team5",
         page: "5.html"
     },
     {
         name: "Team 6",
+        code: "team6",
         page: "6.html"
     },
     {
         name: "Team 7",
+        code: "team7",
         page: "7.html"
     },
     {
         name: "Team 8",
+        code: "team8",
         page: "8.html"
     },
     {
         name: "admin",
+        code: "admin",
         page: "admin.html"
     },
     
@@ -44,18 +53,18 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         
-        const username = document.getElementById('username').value;
+        const userCode = document.getElementById('username').value;
         
-        const team = teams.find(team => team.name === username);
+        const team = teams.find(team => team.code === userCode);
         
         if (team) {
             localStorage.setItem('isAuthenticated', 'true');
-            localStorage.setItem('teamName', username);
+            localStorage.setItem('teamName', team.name);
             localStorage.setItem('teamPage', team.page);
             
             window.location.href = '../groups/' + team.page;
         } else {
-            alert('Неверное имя команды');
+            alert('Неверный код команды');
         }
     });
 });
